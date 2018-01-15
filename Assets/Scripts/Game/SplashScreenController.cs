495405a5316411f0ccd7ba33abf5599d85489756
Mario40000 <mario40000hotmail.com>
2018-01-15 20:35:35 +0100

    Preparado el escenario

---
+++ Assets/Scripts/Game/SplashScreenController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreenController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        StartCoroutine(Counter());
+	
+	}
+	
+	IEnumerator Counter()
+    {
+        for (int i = 0; i < 7;i++)
+        {
+            yield return new WaitForSeconds(1);
+        }
+        SceneManager.LoadScene("MainMenu");
+    }
+}
